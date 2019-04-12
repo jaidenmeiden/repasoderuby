975e609fad2844f7e61d569f9ac1ce90176f9824
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-04-11 21:40:59 -0500

    Actualización Testing en Ruby

--- snake/main.rb
+++ snake/main.rb
@@ -1,3 +1,12 @@
 require "faker"
 
-puts Faker::HarryPotter.spell
+Faker::Name.name      #=> "Christophe Bartell"
+
+Faker::Internet.email #=> "kirsten.greenholt@corkeryfisher.info"
+
+# NOTE: Faker::HarryPotter.spell is deprecated; 
+# use Faker::Movies::HarryPotter.spell instead. 
+# It will be removed on or after 2019-01-01.
+
+# puts Faker::HarryPotter.spell #is deprecated
+puts Faker::Movies::HarryPotter.spell
