2dc37f4fade96a262764b0a10c8ccc399f1a5eef
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-04-13 22:51:17 -0500

    Test de nuestras acciones

--- mysnake/src/actions/actions.rb
+++ mysnake/src/actions/actions.rb
@@ -3,7 +3,7 @@ module Actions
         next_direction = state.next_direction
         next_position = calc_next_position(state)
         #verificar que la siguiente casilla es valida
-        if next_position_is_valid? (state, next_position)
+        if position_is_valid(state, next_position)
             move_snake_to(state, next_position)
         else
             end_game(state)
@@ -11,65 +11,67 @@ module Actions
         #si no es valida -> termina el juego
         #si es valida -> movemos la serpiente
     end
-end
 
-def calc_next_position(state)
-    case state.next_direction
-    when UP
-        #Decrementar la fila
-        return = Model::Coord.new(
-            new_position.row - 1, 
-            new_position.col)
-    when RIGHT
-        #Incrementar la columna
-        return = Model::Coord.new(
-            new_position.row, 
-            new_position.col + 1)
-    when DOWN
-        #Incrementar la fila
-        return = Model::Coord.new(
-            new_position.row + 1, 
-            new_position.col)
-    when LEFT
-        #Decrementar la columna
-        return = Model::Coord.new(
-            new_position.row, 
-            new_position.col - 1)
+    private
+
+    def self.calc_next_position(state)
+        curr_position = state.snake.positions.first
+        case state.next_direction
+        when Model::Direction::UP
+            #Decrementar la fila
+            return Model::Coord.new(
+                curr_position.row - 1, 
+                curr_position.col)
+        when Model::Direction::RIGHT
+            #Incrementar la columna
+            return Model::Coord.new(
+                curr_position.row, 
+                curr_position.col + 1)
+        when Model::Direction::DOWN
+            #Incrementar la fila
+            return Model::Coord.new(
+                curr_position.row + 1, 
+                curr_position.col)
+        when Model::Direction::LEFT
+            #Decrementar la columna
+            return Model::Coord.new(
+                curr_position.row, 
+                curr_position.col - 1)
+        end
     end
-end
 
-def next_position_is_valid(state, position)
-    # Verificar que este sobre la grilla
-    #Opción 1
-    #if position.row < 0 
-    #    || position.row >= state.grid.rows  
-    #    || position.col < 0 
-    #    || position.col >= state.grid.rows
-    #    return false
-    #else
-    #    return true
-    #end
-    
-    #Opción 2
-    is_invalid = ((position.row < 0 
-    || position.row >= state.grid.rows  ) ||
-    (position.col < 0 
-    || position.col >= state.grid.cols  ))
+    def self.position_is_valid(state, position)
+        # Verificar que este sobre la grilla
+        #Opción 1
+        #if position.row >= state.grid.rows ||
+        #  position.row < 0) || 
+        #  (position.col >= state.grid.cols ||
+        #  position.col < 0
+        #    return false
+        #else
+        #    return true
+        #end
+        
+        #Opción 2
+        is_invalid = ((position.row >= state.grid.rows ||
+        position.row < 0) || 
+        (position.col >= state.grid.cols ||
+        position.col < 0))
+        return false if is_invalid
+        # Verificar que no este suyperponiendo a la serpiente
+        return !(state.snake.positions.include? position)
+    end
 
-    return false if is_invalid
-    # Verificar que no este suyperponiendo a la serpiente
-    return !(state.snake.positions.include? position)
-end
+    def self.move_snake_to(state, next_position)
+        # 
+        # [(1,1),(1,0))]
+        new_positions = [next_position] + state.snake.positions[0...-1]
+        state.snake.positions = new_positions
+        state
+    end
 
-def move_snake_to(state, next_position)
-    # 
-    # [(1,1),(1,0))]
-    new_positions = [next_position] + state.snake.positions[0...-1]
-    state.snake.positions = new_positions
-    state
-end
-
-def end_game(state)
-    state.gameover = true
-    state
-end
+    def self.end_game(state)
+        state.gameover = true
+        state
+    end
+end    
