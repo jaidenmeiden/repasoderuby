ad30e96c2adddec0b551870db80f5d740ee689d9
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-04-14 12:49:49 -0500

    Creciendo la serpiente y programando la condición de salida

--- mysnake/src/actions/actions.rb
+++ mysnake/src/actions/actions.rb
@@ -3,7 +3,9 @@ module Actions
         next_direction = state.curr_direction
         next_position = calc_next_position(state)
         #verificar que la siguiente casilla es valida
-        if position_is_valid(state, next_position)
+        if position_is_food(state, next_position)
+            grow_snake_to(state, next_position)
+        elsif position_is_valid(state, next_position)
             move_snake_to(state, next_position)
         else
             end_game(state)
@@ -47,6 +49,17 @@ module Actions
                 curr_position.row, 
                 curr_position.col - 1)
         end
+    end
+
+    def self.position_is_food(state, next_position)           
+        state.food.row == next_position.row && state.food.col == next_position.col
+    end
+
+    def self.grow_snake_to(state, next_position)
+        # TODO; implement
+        new_positions = [next_position] + state.snake.positions
+        state.snake.positions = new_positions
+        state
     end
 
     def self.position_is_valid(state, position)
