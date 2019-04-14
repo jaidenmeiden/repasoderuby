1d0ac3bcfea9c5a766868581adfaef3a36d6cd07
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-04-13 21:22:15 -0500

    Programando el movimiento de nuestro snake

--- mysnake/src/actions/actions.rb
+++ mysnake/src/actions/actions.rb
@@ -17,23 +17,59 @@ def calc_next_position(state)
     case state.next_direction
     when UP
         #Decrementar la fila
-        new_position = Model::Coord.new(
+        return = Model::Coord.new(
             new_position.row - 1, 
             new_position.col)
     when RIGHT
         #Incrementar la columna
-        new_position = Model::Coord.new(
+        return = Model::Coord.new(
             new_position.row, 
             new_position.col + 1)
     when DOWN
         #Incrementar la fila
-        new_position = Model::Coord.new(
+        return = Model::Coord.new(
             new_position.row + 1, 
             new_position.col)
     when LEFT
         #Decrementar la columna
-        new_position = Model::Coord.new(
+        return = Model::Coord.new(
             new_position.row, 
             new_position.col - 1)
     end
+end
+
+def next_position_is_valid(state, position)
+    # Verificar que este sobre la grilla
+    #Opción 1
+    #if position.row < 0 
+    #    || position.row >= state.grid.rows  
+    #    || position.col < 0 
+    #    || position.col >= state.grid.rows
+    #    return false
+    #else
+    #    return true
+    #end
+    
+    #Opción 2
+    is_invalid = ((position.row < 0 
+    || position.row >= state.grid.rows  ) ||
+    (position.col < 0 
+    || position.col >= state.grid.cols  ))
+
+    return false if is_invalid
+    # Verificar que no este suyperponiendo a la serpiente
+    return !(state.snake.positions.include? position)
+end
+
+def move_snake_to(state, next_position)
+    # 
+    # [(1,1),(1,0))]
+    new_positions = [next_position] + state.snake.positions[0...-1]
+    state.snake.positions = new_positions
+    state
+end
+
+def end_game(state)
+    state.gameover = true
+    state
 end
